1ca348fb34293d843631633a228c2195c04dd39e
Hana Elhenawy <m245seed@gmail.com>
2025-10-20 06:34:52 +0100

    Refine language server helpers and document watcher

--- packages/app-vscode-extension/src/content/DocumentWatcher.ts
+++ packages/app-vscode-extension/src/content/DocumentWatcher.ts
@@ -4,14 +4,15 @@ import * as vscode from "vscode";
 import { LanguageServerFacade } from "../language-service-client/LanguageServerFacade";
 
 export class DocumentWatcher {
-  private eventEmitter = new vscode.EventEmitter();
+  private readonly eventEmitter = new vscode.EventEmitter<void>();
   readonly onSuggestionChange = this.eventEmitter.event;
 
   private documentUri: vscode.Uri | undefined = undefined;
+  private documentUriString: string | undefined = undefined;
 
   constructor(private readonly languageServer: LanguageServerFacade) {
     languageServer.onDocumentUpdated(async (documentUri) => {
-      if (documentUri === this.documentUri?.toString()) {
+      if (documentUri === this.documentUriString) {
         this.update();
       }
     });
@@ -22,40 +23,47 @@ export class DocumentWatcher {
   }
 
   setDocumentUri(documentUri: vscode.Uri | undefined) {
+    const newDocumentUriString = documentUri?.toString();
+    if (newDocumentUriString === this.documentUriString) {
+      return;
+    }
+
     this.documentUri = documentUri;
+    this.documentUriString = newDocumentUriString;
     this.update();
   }
 
   async getFunctionElements(): Promise<Array<p42.FunctionElement> | undefined> {
-    return this.documentUri != null
-      ? await this.languageServer.getFunctionElements(
-          this.documentUri.toString()
-        )
-      : undefined;
+    return this.withDocumentUri((uri) =>
+      this.languageServer.getFunctionElements(uri)
+    );
   }
 
   async getSuggestions(): Promise<
     Array<SerializedSuggestionCodeAssist> | undefined
   > {
-    return this.documentUri != null
-      ? await this.languageServer.getSuggestions(this.documentUri.toString())
-      : undefined;
+    return this.withDocumentUri((uri) =>
+      this.languageServer.getSuggestions(uri)
+    );
   }
 
   async getCodeAssistDiff(
     codeAssistId: string,
     contextLines?: number | undefined
   ) {
-    return this.documentUri != null
-      ? await this.languageServer.getCodeAssistDiff(
-          this.documentUri.toString(),
-          codeAssistId,
-          contextLines
-        )
-      : undefined;
+    return this.withDocumentUri((uri) =>
+      this.languageServer.getCodeAssistDiff(uri, codeAssistId, contextLines)
+    );
+  }
+
+  private async withDocumentUri<T>(
+    callback: (documentUri: string) => Promise<T>
+  ): Promise<T | undefined> {
+    const { documentUriString } = this;
+    return documentUriString != null ? await callback(documentUriString) : undefined;
   }
 
   private update() {
-    this.eventEmitter.fire(undefined);
+    this.eventEmitter.fire();
   }
 }
